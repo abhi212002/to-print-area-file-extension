5b8e71410f54653bfc57ff9cbf0ea88430cb2cdb
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2020-06-11 15:24:03 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -2,5 +2,5 @@ filename=input("Input the Filename : ")
 a=filename.split(".")
 if a[-1]=='py':
    print("The extension of the file is :" +"'python'")
-else
+else:
    print("The extension of the file is :",repr(a[-1]))
